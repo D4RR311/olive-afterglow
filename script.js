e2b9a694b91aea4b1b00372e0da033563b08048b
D4RR311 <117194586+D4RR311@users.noreply.github.com>
2026-09-16 07:23:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 const CONFIG = {
 
     APPS_SCRIPT_URL:
-        "https://script.google.com/macros/s/AKfycbxUnfteSZXvkX3ylA6MZ81XLhvm9F1z_xbJCoILgAJkL19A7upvuC5YPKLg_enGPuTvGA/exec",
+        "https://docs.google.com/spreadsheets/d/1BVRJebSQqGdtCIm1HKWyKrUKie93FRmuT3NHAJ0OWts/edit?usp=sharing",
 
     WEDDING_DATE:
         "2026-09-17T09:15:00+07:00",
